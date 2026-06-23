d4a8fbd12d67e6d06dcd9067c16647b1f2f639c4
Aman Sahu <amansahuxy@gmail.com>
2026-06-23 13:41:34 +0530

    Add counter app with event listeners

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 let count=0;
+
+let countText=document.getElementById("count");
  const incBtn=document.getElementById("incr");
  const dcrBtn=document.getElementById("dcr");
  const rstBtn=document.getElementById("rst");
 
- 
+ incBtn.addEventListener("click",()=>{
+    count++;
+    countText.innerText=count;
+    
+ })
+
+  dcrBtn.addEventListener("click",()=>{
+    count--;
+    countText.innerText=count;
+    
+ })
+  rstBtn.addEventListener("click",()=>{
+    count=0;
+    countText.innerText=count;
+    
+ })
